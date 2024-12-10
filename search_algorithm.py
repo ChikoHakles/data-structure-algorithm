551d734417006b01345471c4c4b3db9cab1d81a7
ChikoHakles <>
2024-12-10 21:44:54 +0700

    Adding comments

--- search_algorithm.py
+++ search_algorithm.py
@@ -4,46 +4,40 @@ def append_data(count):
         data.append(i)
     return data
 
-data = append_data(1000)
-
+#mencari data secara berurutan dari index 0
 def seq_search(data, search):
     i = -1
     loop = 0
     while 1:
+        #naik 1 index setiap loop
         i = i+1
         loop = loop + 1
         if data[i] == search:
+            #selesai saat mendapat angka yang sesuai
             break
     return i, loop
 
+#mencari data dari tengah, dan akan mengarah ke angka yang lebih kecil / besar sesuai komparasi pertama
 def binary_search(data, search):
+    #index awal = 0, index akhir = jumlah data, index tengah = jumlah data dibagi 2
     min_index = 0
     max_index = len(data)
     to_search = int(max_index/2)
     i = -1
     loop = 0
     while 1:
+        #setiap perulangan akan cek, apakah angka sama, lebih kecil, atau lebih besar
         loop = loop + 1
+        #jika lebih kecil, maka akan ke kiri garis bilangan (cari yang lebih kecil dari angka tengah)
         if search < data[to_search]:
             max_index = to_search
             to_search = int(max_index/2)
+        #jika lebih besar, maka akan ke kanan garis bilangan (cari yang lebih besar dari angka tengah)
         elif search > data[to_search]:
             min_index = to_search
             to_search = min_index + int((max_index - min_index)/2)
+        #jika sama, maka itulah angka yg dicari
         else :
             i = to_search
             break
     return i, loop
-
-def main():
-    print(data)
-    inp  = int(input("Input an int : "))
-    res_seq = seq_search(data, inp)
-    res_bin = binary_search(data, inp)
-    print("Sequential Search : Integer", inp, "Found in index", res_seq[0]) if res_seq[0] != -1 else print("Integer not found")
-    print("Loop :", res_seq[1], "times")
-    print("Binary Search : Integer", inp, "Found in index", res_bin[0]) if res_bin[0] != -1 else print("Integer not found")
-    print("Loop :", res_bin[1], "times")
-
-if __name__ == "__main__":
-    main()
